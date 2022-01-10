4c26fa948d5a334ebaa9e3ea7339316f0c223887
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-01-10 09:41:18 +0400

    feat: add layout component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent],
   imports: [BrowserModule, BrowserAnimationsModule, RouterModule],
+  exports: [LayoutComponent],
 })
 export class CoreModule {}
