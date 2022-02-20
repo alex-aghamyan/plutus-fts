62f4630d28c190430b2c8143e7c476946abbf2ef
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-02-20 13:33:36 +0400

    fix(auth): fix error catching when logging in

--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
-import { IUser } from '../../core/models/user.model';
-import { AuthService } from '../../core/services/auth.service';
+import { catchError, from, map, of, switchMap } from 'rxjs';
+import { IUser } from 'src/app/core/models/user.model';
+import { AuthService } from 'src/app/core/services/auth.service';
 import * as authActions from '../actions/auth.actions';
 
 @Injectable()
@@ -10,10 +10,15 @@ export class AuthEffects {
   login$ = createEffect(() =>
     this.actions$.pipe(
       ofType(authActions.loginAttempt),
-      map(() => this.authService.signInWithGoogle()),
       switchMap(() =>
-        this.authService.getUser().pipe(
-          map((user: IUser) => authActions.loginSuccess({ data: user })),
+        from(this.authService.signInWithGoogle()).pipe(
+          switchMap(() =>
+            this.authService
+              .getUser()
+              .pipe(
+                map((user: IUser) => authActions.loginSuccess({ data: user }))
+              )
+          ),
           catchError((err: Error) =>
             of(authActions.loginFailure({ error: err }))
           )
