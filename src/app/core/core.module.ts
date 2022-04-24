cff78dfdd9be06675125b1367dac2ec42fdfd404
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-04-24 20:30:08 +0400

    refactor(layout): move RouterModule from Core to Layout module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 import { RootStoreModule } from './modules/root-store.module';
 import { FirebaseModule } from './modules/firebase.module';
 import { LayoutModule } from './layout/layout.module';
@@ -10,7 +9,6 @@ import { LayoutModule } from './layout/layout.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule,
     RootStoreModule,
     FirebaseModule,
   ],
--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
