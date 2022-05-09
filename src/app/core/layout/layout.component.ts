70bb9472133f024acb4cc64affbe18232293a972
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-05-09 20:06:57 +0400

    feat(layout-footer): integrate footer actions component
    - add the component to the layout
    - handle the actions from the component

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { FooterAction } from '../models/footer-actions.model';
 import { checkLayout } from '../store/actions/layout.actions';
 import { LayoutState } from '../store/reducers/layout.reducer';
 import { selectLayout } from '../store/selectors/layout.selectors';
@@ -12,6 +13,7 @@ import { selectLayout } from '../store/selectors/layout.selectors';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent implements OnInit {
+  openMenu = false;
   layout$: Observable<LayoutState> = this.store.select(selectLayout);
 
   constructor(private store: Store) {}
@@ -19,4 +21,28 @@ export class LayoutComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(checkLayout());
   }
+
+  handleFooterAction(action: FooterAction): void {
+    switch (action) {
+      case 'toggleMenu':
+        this.toggleMenu();
+        break;
+
+      case 'addIncomeOrExpense':
+        this.addIncomeOrExpense();
+        break;
+
+      case 'openProfile':
+        this.openProfile();
+        break;
+    }
+  }
+
+  toggleMenu(): void {
+    this.openMenu = !this.openMenu;
+  }
+
+  addIncomeOrExpense(): void {}
+
+  openProfile(): void {}
 }
