39d0ca21a6d782c4174fca72236a63494a5529e6
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-05-09 20:08:47 +0400

    feat(layout-menu): add main menu component

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -5,19 +5,22 @@ import { RouterModule } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 
 import { LayoutComponent } from './layout.component';
 import { FooterActionsComponent } from './components/footer-actions/footer-actions.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
-  declarations: [LayoutComponent, FooterActionsComponent],
+  declarations: [LayoutComponent, FooterActionsComponent, MainMenuComponent],
   imports: [
     CommonModule,
     RouterModule,
     NzLayoutModule,
     NzButtonModule,
     NzIconModule,
+    NzMenuModule,
     NzAvatarModule,
   ],
   exports: [LayoutComponent],
