4611d127acce91df49a7ddbe9e86a3497d3a5a89
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-07-02 20:12:23 +0400

    refactor(ngrx-eslint): remove layout and user checking actions

--- src/app/core/store/actions/layout.actions.ts
+++ src/app/core/store/actions/layout.actions.ts
@@ -1,9 +1,8 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 
 export const layoutActions = createActionGroup({
   source: 'Layout',
   events: {
-    'Check Layout': emptyProps(),
     'Set Layout As Mobile': props<{ isMobile: boolean }>(),
   },
 });
