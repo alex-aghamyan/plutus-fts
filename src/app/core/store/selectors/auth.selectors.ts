35a9ff2cd9ea30f7431433e5c2ebe5650840b577
alex-aghamyan <alex.aghamyan.work@gmail.com>
2022-08-25 12:22:04 +0400

    refactor(ngrx): remove unnecessary selector files

--- src/app/core/store/selectors/auth.selectors.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from '@ngrx/store';
-import { authFeature } from '@fts-store/reducers';
-
-export const selectUserViewModel = createSelector(
-  authFeature.selectUser,
-  (user) => (user)
-);
