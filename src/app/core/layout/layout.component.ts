1b32b2dda76ca1fadd005e399d216992fa92530c
alex-aghamyan <alex.aghamyan.work@gmail.com>
2023-01-21 18:01:51 +0400

    refactor(layout, NgRx): move `initial view selector` to `layout`

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 import { FooterAction } from '../models/footer-actions.model';
 import { IInitialViewModel } from '../models/initial-view-model.model';
 import { coreActions } from '@fts-store/core';
-import { selectInitialViewModel } from '@fts-store/core';
+import { selectInitialViewModel } from '@fts-store/layout';
 import { FooterActionsComponent } from './components/footer-actions/footer-actions.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
